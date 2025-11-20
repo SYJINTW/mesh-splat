d9298f427954cd3c419dea12020f5142eda17ba5
samtech37 <samkondori@gmail.com>
2025-11-20 13:07:49 +0000

    update plotter

--- metrics.py
+++ metrics.py
@@ -79,9 +79,10 @@ def evaluate(gs_type, model_paths):
                     ssims.append(ssim(renders[idx], gts[idx]))
                     psnrs.append(psnr(renders[idx], gts[idx]))
                     
+                    lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
+                    
                     # [NOTE] skip LPIPS to save time for now
-                    # lpipss.append(lpips(renders[idx], gts[idx], net_type='vgg'))
-                    lpipss.append(-1.0)
+                    # lpipss.append(-1.0)
                     
                     
 
--- shell_scripts/copy_results_data.sh
+++ shell_scripts/copy_results_data.sh
@@ -1,27 +1,31 @@
 
-# BASE=/mnt/data1/syjintw/NEU/mesh-splat/output/main_nerfsynthetic
-# SCENES=("lego" "ficus" "hotdog" "mic" "ship")
+BASE=/mnt/data1/syjintw/NEU/mesh-splat/output/main_nerfsynthetic
+# BASE=/mnt/data1/syjintw/NEU/mesh-splat-mixed/output/main_nerfsynthetic
+SCENES=("lego" "ficus" "hotdog" "mic" "ship")
 
-# for SCENE in ${SCENES[@]}
-# do
-#     echo "Copying per_view_gs_mesh.json files for scene: $SCENE"
+
+echo "copying mesh_splat results data to ./data/ ..."
+
+for SCENE in ${SCENES[@]}
+do
+    echo "Copying per_view_gs_mesh.json files for scene: $SCENE"
     
-#     # Find all per_view_gs_mesh.json files in the scene directory
-#     find ${BASE}/${SCENE} -name "per_view_gs_mesh.json" | while read json_file; do
-#         # Get the relative path from BASE
-#         rel_path=${json_file#${BASE}/}
+    # Find all per_view_gs_mesh.json files in the scene directory
+    find ${BASE}/${SCENE} -name "per_view_gs_mesh.json" | while read json_file; do
+        # Get the relative path from BASE
+        rel_path=${json_file#${BASE}/}
         
-#         # Create destination directory structure
-#         dest_dir=./data/$(dirname $rel_path)
-#         mkdir -p $dest_dir
+        # Create destination directory structure
+        dest_dir=./data/$(dirname $rel_path)
+        mkdir -p $dest_dir
         
-#         # Copy the file
-#         cp $json_file $dest_dir/
-#         echo "  Copied: $rel_path"
-#     done
-# done
+        # Copy the file
+        cp $json_file $dest_dir/
+        echo "  Copied: $rel_path"
+    done
+done
 
-# echo "Done! All per_view_gs_mesh.json files copied to ./data/"
+echo "Done! All per_view_gs_mesh.json files copied to ./data/"
 
 
 BASE=/mnt/data1/syjintw/NEU/mesh-splat/output/main_mipnerf360
@@ -38,7 +42,7 @@ do
         rel_path=${json_file#${BASE}/}
         
         # Create destination directory structure
-        dest_dir=./data/bicycle_exps/$(dirname $rel_path)
+        dest_dir=./data/$(dirname $rel_path)
         mkdir -p $dest_dir
         
         # Copy the file
@@ -48,3 +52,63 @@ do
 done
 
 echo "Done! All per_view_gs_mesh.json files copied to ./data/bicycle_exps/"
+
+echo "\n\n\n"
+
+
+
+# ============================================================
+
+GAMES_DATA_DIR="./data/games_results"
+echo "Copying GaMeS results data to $GAMES_DATA_DIR/ ..."
+
+
+BASE=/mnt/data1/syjintw/NEU/mesh-splat-games/output/main_nerfsynthetic
+SCENES=("lego" "ficus" "hotdog" "mic" "ship")
+
+for SCENE in ${SCENES[@]}
+do
+    echo "Copying per_view_gs_mesh.json files for scene: $SCENE"
+    
+    # Find all per_view_gs_mesh.json files in the scene directory
+    find ${BASE}/${SCENE} -name "per_view_gs_mesh.json" | while read json_file; do
+        # Get the relative path from BASE
+        rel_path=${json_file#${BASE}/}
+        
+        # Create destination directory structure
+        dest_dir=$GAMES_DATA_DIR/$(dirname $rel_path)
+        mkdir -p $dest_dir
+        
+        # Copy the file
+        cp $json_file $dest_dir/
+        echo "  Copied: $rel_path"
+    done
+done
+
+echo "Done! All per_view_gs_mesh.json files copied to ./data/"
+
+
+BASE=/mnt/data1/syjintw/NEU/mesh-splat-games/output/main_mipnerf360
+SCENES=("bicycle" "bicycle-dw10" "bicycle-dw30" "bicycle-dw50")
+
+for SCENE in ${SCENES[@]}
+do
+    echo "Copying per_view_gs_mesh.json files for scene: $SCENE"
+    
+    # Find all per_view_gs_mesh.json files in the scene directory
+    find ${BASE}/${SCENE} -name "per_view_gs_mesh.json" | while read json_file; do
+        # Get the relative path from BASE
+        rel_path=${json_file#${BASE}/}
+        
+        # Create destination directory structure
+        dest_dir=$GAMES_DATA_DIR/$(dirname $rel_path)
+        mkdir -p $dest_dir
+        
+        # Copy the file
+        cp $json_file $dest_dir/
+        echo "  Copied: $rel_path"
+    done
+done
+
+echo "Done ! All per_view_gs_mesh.json files copied to $GAMES_DATA_DIR/"
+
